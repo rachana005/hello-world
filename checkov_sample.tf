2beaeedfd3f58b0b95be983a58e9de31c30a9fc0
rachana-sharma <rachana.sharma@powerupcloud.com>
2021-01-04 15:43:30 +0530

    removed checkov_sample

--- checkov_sample.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "foo-bucket" {
-  region        = var.region
-    #checkov:skip=CKV_AWS_20:The bucket is a public static content host
-  bucket        = local.bucket_name
-  force_destroy = true
-  acl           = "public-read"
-}
